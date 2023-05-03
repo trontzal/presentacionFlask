8dd99d01abae00d7111f57c6ba8b3f91f908e825
trontzal <trontzal@example.com>
2023-05-03 22:28:57 +0200

    first commit

---
+++ ejmploConFlask/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def primerPaso():
+    return '¡Hola, mundo!'
+
+@app.route('/vista')
+def rutas():
+    return 'Esto es una vista'
+
+@app.route('/plantillas')
+def plantillas():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5001)
